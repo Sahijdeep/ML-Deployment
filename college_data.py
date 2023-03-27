dd2a085805efa45530b96f2385ccb61876b61be2
Sahijdeep Aujla <117302675+Sahijdeep@users.noreply.github.com>
2023-03-27 15:40:30 -0600

    Update college_data.py

--- college_data.py
+++ college_data.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
 df = pd.read_csv("data.csv")
 df.describe().T
 sns.countplot(data=df, x='private')
